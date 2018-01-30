a4060fb9bb69258d1a7a2e7a0344ff26d3b39ed2
Thijs Raymakers <thijs@raymakers.nl>
2018-01-30 23:58:41 +0100

    Improve formula to take the Februari Purge into account

--- index.js
+++ index.js
@@ -17,14 +17,22 @@ app.get('/dropout', function(req, res) {
     var amount_of_students_on_day_1 = 433;
     var percentage_that_drops_out = 0.50;
 
-    var students_per_day_that_drop_out = (amount_of_students_on_day_1/amount_of_days) * percentage_that_drops_out;
+    var scaling_factor = 200;
 
     var now = new Date();
     var starting_date = new Date("2017-09-04");
 
     var current_day_in_year = Math.round(Math.abs((now.getTime() - starting_date.getTime())/(86400000)));
 
-    var dropped_out = Math.floor(current_day_in_year * students_per_day_that_drop_out);
+    var dropped_out_ratio = Math.log(current_day_in_year + 1) / (2 * Math.log(scaling_factor));
+
+    if(current_day_in_year > (amount_of_days/2)) {
+        dropped_out_ratio = dropped_out_ratio + (Math.log(current_day_in_year + 1 - (amount_of_days/2))) / (2*Math.log(scaling_factor));
+    }
+
+    dropped_out_ratio = dropped_out_ratio / 2;
+
+    var dropped_out = (dropped_out_ratio * amount_of_students_on_day_1).toFixed(2);
 
     res.marko(dropout, {
         dropouts: dropped_out
