1887f2b239d4e1430cc91645780c60cad4901a8b
Thijs Raymakers <thijs@raymakers.nl>
2018-02-27 00:16:17 +0100

    Add more situations

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -16,9 +16,9 @@ var situations = {
         '14': "Er gaat weer eens iets mis in de dia's",
         '15': "Iemands telefoon gaat af",
         '16': "Iemand gooit de microfoon verkeerd",
-        '17': "Freek vonk kijkt boos om",
+        '17': "Freek Vonk kijkt boos om",
         '18': "Mensen die aan het gamen zijn tijdens college",
-        '19': "Ze dreigt met het niet afkrijgen van de powerpoint",
+        '19': "Ze dreigt met het niet afkrijgen van de PowerPoint™",
         '20': "De microfoon werkt niet",
         '21': "Antwoord op een vraag is fout",
         '22': "Niemand zit naast freek vonk",
@@ -28,14 +28,19 @@ var situations = {
         '26': "'Ask your neighbour'",
         '27': "'You can do this at home'",
         '28': "Three/Tree",
-        '29': "Node/Note",
+        '29': "Hieps",
         '30': "De lecture is weer exclusief voor TW",
         "31": "Niemand durft bingo te roepen",
         "32": "Iemand roept bingo",
         "33": "Iemand stelt een vraag over de stof van volgende week",
         "34": "Microfoon komt tegen iemands hoofd aan",
         "35": "Niemand geeft antwoord op de gestelde vraag",
-        "36": "Microfoon valt op de grond"
+        "36": "Microfoon valt op de grond",
+        "37": "Drie mensen op een rij hebben hun laptop open staan",
+        "38": "Steks",
+        "39": "Slide wordt geskipt",
+        "40": "In de pauze lopen mensen naar voren om vragen te stellen",
+        "41": "Melvin neemt het college over"
 };
 
 module.exports = {
