9d65394bda9d77e33f1fe0a381c4fb686309feb0
Thijs Raymakers <thijs@raymakers.nl>
2018-05-24 23:35:54 +0200

    Add Starcraft game number API

--- api.js
+++ api.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 var dropout = require('./dropout/dropout');
 router.get('/dropouts', function(req, res) {
     res.send(dropout.getDropouts() + "");
 });
 
+
+/* BINGO CURRENT PLAYERS*/
 var active_clients = {};
 var bingo = require('./bingo/bingo');
 router.post('/alive', function(req, res) {
@@ -29,5 +33,16 @@ setInterval(function() {
         } 
     }
 }, 5000);
+
+/* Starcraft current game*/
+const LOCATION_OF_GAME_NUMBER = "body>table:nth-child(1)>tbody>tr:nth-child(3)>td:nth-child(3)";
+router.get('/starcraft/current_game', function(req, res) {
+    JSDOM.fromFile("files/starcraft/index.html", options).then(dom => {
+        var innerText = dom.window.document.querySelectorAll(LOCATION_OF_GAME_NUMBER)[0].innerText;
+        var index = innerText.indexOf("/");      
+        var game_number = innerText.substring(0,index-1);
+        res.send(game_number);
+    });
+}); 
 
 module.exports = router;
