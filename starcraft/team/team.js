2b068b4e021f664702b2586dafe28f71ce94385e
Thijs Raymakers <thijs@raymakers.nl>
2018-05-31 17:16:25 +0200

    Add Team class

---
+++ starcraft/team/team.js
@@ -0,0 +1,15 @@
+class Team {
+    constructor(name) {
+        this._name = name;
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    setName(name) {
+        this._name = name;
+    }
+}
+
+module.exports = { Team };
