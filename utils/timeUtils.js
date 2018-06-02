449930403b994447b6d9ea691eac3aae479c5ffe
Thijs Raymakers <thijs@raymakers.nl>
2018-06-02 15:55:42 +0200

    Add timeUtils class

---
+++ utils/timeUtils.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    formatSecondsToDate: (seconds) => {
+        var h = Math.floor(seconds / 3600);
+        var m = Math.floor((seconds - (h * 3600)) / 60);
+        var s = Math.floor(seconds - (h * 3600) - (m * 60));
+        return [h, m, s].map((x, i) => x.toString().padStart(2, "0") + "hms"[i]).join("");
+    }
+
+}
