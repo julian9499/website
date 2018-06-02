69f4e8028f8c2f9a278e15144e7eea5f516b2975
Thijs Raymakers <thijs@raymakers.nl>
2018-06-02 13:48:05 +0200

    Add team class

--- starcraft/team/team.js
+++ starcraft/team/team.js
@@ -1,6 +1,19 @@
 class Team {
-    constructor(name) {
+    constructor(name, race, games, win, loss, winpercentage, avgTime, hour, crash, timeout) {
         this._name = name;
+        this._race = race;
+        this._games = games;
+        this._win = win;
+        this._loss = loss;
+        this._winpercentage = winpercentage;
+        this._avgTime = avgTime;
+        this._hour = hour;
+        this._crash = crash;
+        this._timeout = timeout;
+    }
+
+    static invalid() {
+        return new this("Unknown", "unknown", 0, 0, 0, 0, 0, 0, 0, 0);
     }
 
     getName() {
@@ -9,6 +22,26 @@ class Team {
 
     setName(name) {
         this._name = name;
+    }
+
+    getRace() {
+        return this._race;
+    }
+
+    setRace(race) {
+        this._race = race;
+    }
+
+    getWinrate() {
+        return this._winpercentage;
+    }
+
+    toString() {
+        return this._name + " (" + this.raceToLetter() + " - " + this._win + "/" + this._games + ")"
+    }
+
+    raceToLetter() {
+        return this._race.charAt(0).toUpperCase();
     }
 }
 
