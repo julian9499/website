3b346229cd86f577104cb231ec6050f6de0f3244
Thijs Raymakers <thijs@raymakers.nl>
2018-06-02 17:02:45 +0200

    Enable CORS

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const bodyParser = require('body-parser');
 
 var app = express();
  
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var path = require('path')
 app.use('/assets', express.static(path.join(__dirname, 'files/assets')))
 
