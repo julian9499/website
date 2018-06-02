af64db93ec9bf6dc0c390453e04e1b204c1f5d1b
Thijs Raymakers <thijs@raymakers.nl>
2018-06-02 16:13:28 +0200

    Remove hours if game is less than 1 hour away

--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -3,8 +3,11 @@ module.exports = {
     formatSecondsToDate: (seconds) => {
         var h = Math.floor(seconds / 3600);
         var m = Math.floor((seconds - (h * 3600)) / 60);
-        // var s = Math.floor(seconds - (h * 3600) - (m * 60));
-        return [h, m].map((x, i) => x.toString().padStart(2, "0") + "hm"[i]).join("");
+        if(h != 0) {
+            return [h, m].map((x, i) => x.toString().padStart(2, "0") + "hm"[i]).join("");
+        } else {
+            return m + "m";
+        }
     }
 
 }
